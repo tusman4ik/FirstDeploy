09c6555e465253d16b3f83eac8b65f3251446318
tusman4ik <tusman4ik@gmail.com>
2023-08-04 12:06:31 +0300

    create project

---
+++ src/main/java/com/example/FirstDeploy/FirstDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstDeploy/FirstDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
